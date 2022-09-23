0358503095acb618fd69f2bde282a2ea4f406e68
GIRALD0 <mateogiraldost@gmail.com>
2022-09-23 15:19:13 -0500

    git commit -m -a "Add animations on scroll and fix responsive"

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // PRELOADER
-$(window).load( function () {
-    $( "#loading" ).fadeOut( 500 );
+$(window).load(function () {
+    $("#loading").fadeOut(500);
 });
 
 // NAVBAR
@@ -11,23 +11,43 @@ const sections1 = document.querySelector('.nav-text1');
 const sections2 = document.querySelector('.nav-text2');
 const sections3 = document.querySelector('.nav-text3');
 
-var tl = gsap.timeline({ defaults: { duration: 1, ease: 'expo.inOut' } });
+var tl = gsap.timeline({
+    defaults: {
+        duration: 1,
+        ease: 'expo.inOut'
+    }
+});
 
 open.addEventListener('click', () => {
-	if (tl.reversed()) {
-		tl.play();
-	} else {
-		tl.to('nav', { right: 0 })
-		.to('nav', { height: '100vh' }, '-=.1')
-		.to('nav ul li a', { opacity: 1, pointerEvents: 'all', stagger: .2 }, '-=.9')
-		.to('.close', { opacity: 1, pointerEvents: 'all' }, "-=.8")
-		.to('nav h2', { opacity: 1 }, '-=1')
-        .to('svg', { opacity: 1 }, '-=1')
-	}
+    if (tl.reversed()) {
+        tl.play();
+    } else {
+        tl.to('nav', {
+                right: 0
+            })
+            .to('nav', {
+                height: '100vh'
+            }, '-=.1')
+            .to('nav ul li a', {
+                opacity: 1,
+                pointerEvents: 'all',
+                stagger: .2
+            }, '-=.9')
+            .to('.close', {
+                opacity: 1,
+                pointerEvents: 'all'
+            }, "-=.8")
+            .to('nav h2', {
+                opacity: 1
+            }, '-=1')
+            .to('svg', {
+                opacity: 1
+            }, '-=1')
+    }
 });
 
 close.addEventListener('click', () => {
-	tl.reverse();
+    tl.reverse();
 });
 
 sections.addEventListener('click', () => {
@@ -62,3 +82,55 @@ const typed = new Typed('.typed', {
     cursorChar: '|',
     contentType: 'html',
 })
+
+// SCROLL FADE OUT LOGO
+
+let lastScroll = $(window).scrollTop();
+
+$(window).scroll(function () {
+    const currentScroll = $(window).scrollTop();
+    if (currentScroll > lastScroll) {
+        // scroll down
+        $('#logo').removeClass().addClass('hidden');
+    } else {
+        // scroll up
+        $('#logo').removeClass().addClass('visible');
+    }
+
+    // scroll update
+    lastScroll = currentScroll <= 0 ? 0 : currentScroll;
+});
+
+// SCROLL DOWN FADE IN EFFECT
+$(window).on("load",function() {
+    function fade() {
+        var animation_height = $(window).innerHeight() * 0.25;
+        var ratio = Math.round( (1 / animation_height) * 10000 ) / 10000;
+
+        $('.fadeIn').each(function() {
+
+            var objectTop = $(this).offset().top;
+            var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+
+            if ( objectTop < windowBottom ) {
+                if ( objectTop < windowBottom - animation_height ) {
+                    $(this).css( {
+                        transition: 'opacity 1s linear',
+                        opacity: 1
+                    } );
+
+                } else {
+                    $(this).css( {
+                        transition: 'opacity 1s linear',
+                        opacity: (windowBottom - objectTop) * ratio
+                    } );
+                }
+            } else {
+                $(this).css( 'opacity', 0 );
+            }
+        });
+    }
+    $('.fadeIn').css( 'opacity', 0 );
+    fade();
+    $(window).scroll(function() {fade();});
+});
